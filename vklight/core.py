3d2493695ff5a7c3c6ac00f13d4b3db05adcfd28
Ivan <24375241+keyzt@users.noreply.github.com>
2020-11-13 00:18:49 +0700

    Up 1.3.8

--- vklight/core.py
+++ vklight/core.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 """
 Light wrapper for VK's API
 """
 
-
 import requests
-
 
 host = "api.vk.me"
 proxyHost = "v-api-proxy.xtrafrancyz.net"
@@ -17,9 +14,9 @@ userAgent = {'user-agent': 'VKAndroidApp/6.13.1-6127 (Android 10.0; SDK 29; arm6
 
 
 class VKLight:
-    """VKLigth - Light wrapper for VK's API"""
+    """VKLight - Light wrapper for VK's API"""
 
-    def __init__(self, param= dict()):
+    def __init__(self, param=None):
         """
         :param: Dictionary including  fields such as 'access_token' (required), 'v' and etc.
 
@@ -27,32 +24,39 @@ class VKLight:
         """
         super(VKLight, self).__init__()
 
+        if param is None:
+            param = dict()
         self.access_token = self.__v("access_token", param) or ""
         self.apiVersion = self.__v("v", param) or apiVersion
         self.lang = self.__v("lang", param) or "en"
-        
+
         self.host = proxyHost if self.__v("proxy", param) else host
         self.host = param['host'] if self.__v("host", param) else self.host
         self.baseURL = f"https://{self.host}/method/"
         self.url_param = dict(lang=self.lang, v=self.apiVersion)
 
-    def __call__(self, method:str, args=dict()):
+    def __call__(self, method: str, args=None):
+        if args is None:
+            args = dict()
         return self.call(method, args)
 
-    def call(self, method:str, args=dict()) -> dict:
+    def call(self, method: str, args=None) -> dict:
         """
         Calls VK API methods
+
         :param method: VK API method name.
         :param args: arguments of this method.
         """
+        if args is None:
+            args = dict()
         args['access_token'] = self.access_token
 
         try:
             resp = requests.post(
-                f"{self.baseURL}{method}", 
+                f"{self.baseURL}{method}",
                 data=args,
-                params=self.url_param, 
-                headers=userAgent, 
+                params=self.url_param,
+                headers=userAgent,
                 timeout=10
             ).json()
 
@@ -63,8 +67,8 @@ class VKLight:
             raise VKLightError(resp['error']['error_code'], resp['error']['error_msg'])
 
         return resp
-    
-    def execute(self, code:str=""):
+
+    def execute(self, code: str = ""):
         """
         Calls Execute method
         Learn More: https://vk.com/dev/execute
@@ -73,13 +77,14 @@ class VKLight:
         """
         return self.call("execute", {"code": code})
 
-    def __v(self, key, dict_data: dict):
+    @staticmethod
+    def __v(key, dict_data: dict):
         return dict_data[key] if key in dict_data else ""
 
 
-
 class VKLightError(Exception):
-    """ VKLight Exception for errros from VK API's """
+    """ VKLight Exception for errors from VK API's """
+
     def __init__(self, error_code, message):
         """
         :param error_code: Code of Error
